b28b876dc3c804c4af1fdf77edb56cdc46aec09b
Marto_0 <martvanenck1@gmail.com>
2019-10-12 01:44:14 +0200

    compiled/index.js

---
+++ compiled/index.js
@@ -0,0 +1,59 @@
+var $ = require('jquery');
+var dialog = require('electron').dialog;
+var electron = require('electron');
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+var globalShortcut = electron.globalShortcut;
+var ipcMain = electron.ipcMain;
+var autoUpdater = require("electron-updater").autoUpdater;
+require('v8-compile-cache');
+try {
+    require('electron-reload')(__dirname, {
+        ignored: /node_modules|storage|[\/\\]\./
+    });
+}
+catch (e) { }
+app.on('ready', function () {
+    var window = new BrowserWindow({
+        width: 400,
+        height: 800,
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true,
+            nodeIntegrationInWorker: true
+        },
+        icon: './icon.ico'
+    });
+    window.setMenu(null);
+    window.setMaximizable(false);
+    window.setResizable(false);
+    window.loadFile('./compiled/src/index.html');
+    if (isDev()) {
+        window.webContents.once('dom-ready', function () {
+            window.webContents.openDevTools();
+        });
+    }
+    setTimeout(function () {
+        autoUpdater.checkForUpdatesAndNotify();
+    }, 2000);
+    window.on('closed', function () { win = null; });
+    globalShortcut.register('MediaPlayPause', function () {
+        window.webContents.executeJavaScript("onMediaPlayPause()");
+    });
+    globalShortcut.register('MediaPreviousTrack', function () {
+        window.webContents.executeJavaScript("onMediaPreviousTrack()");
+    });
+    globalShortcut.register('MediaNextTrack', function () {
+        window.webContents.executeJavaScript("onMediaNextTrack()");
+    });
+});
+function isDev() {
+    try {
+        require('electron-builder');
+    }
+    catch (e) {
+        return false;
+    }
+    return true;
+}
+//# sourceMappingURL=index.js.map
