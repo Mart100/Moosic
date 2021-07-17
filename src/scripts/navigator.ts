9f0f3eaff923a353716982acda9f664769798a55
Marto_0 <martvanenck1@gmail.com>
2021-07-17 20:03:55 +0200

    some style changes

--- src/scripts/navigator.ts
+++ src/scripts/navigator.ts
@@ -20,7 +20,7 @@ $(() => {
 
     if(name == 'search') {
       $('#search .songs').show()
-      $('#search').fadeIn()
+      $('#search').fadeIn(150)
       $('#search .input').focus()
       if(searchResults) showSongs(searchResults, {topBar: false})
     }
@@ -28,14 +28,14 @@ $(() => {
     if(name == 'mySongs') {
       $('#mySongs .songs').hide()
       $('#mySongsMenu').show()
-      $('#mySongs').fadeIn()
+      $('#mySongs').fadeIn(150)
       loadCollections()
     }
 
     if(name == 'settings') {
-      $('#settings').fadeIn()
+      $('#settings').fadeIn(150)
       $('#settings .thing1').hide()
-      $('#settings-main').fadeIn(250)
+      $('#settings-main').fadeIn(150)
 
       // enable/disable discord RPC
       if(database.settings.discordRPC) $('#discordRPC-button').html('Disable discord RPC')
